da7b331ed9841bc8d66996790d89390a42c99240
noureddine.bensadok@ouidou.fr <noureddine.bensadok@ouidou.fr>
2025-02-04 19:34:27 +0100

    hashBrowser

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import KitchenDashboard from "./pages/restaurant/KitchenDashboard";
